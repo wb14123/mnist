9bf0e00d809879d9dd387b651700baf79ddc9dcf
Bin Wang <wbin00@gmail.com>
2015-03-20 20:51:50 +0800

    Fix dataset format
    1. Change Lable 0 as 1, in order to use ClassNLLCriterio.
    2. Chnage size to size(), in order to fit torch's dataset format.
    3. Make the input a 3d tensor, in order to make it easier to use in a
    CNN.

--- init.lua
+++ init.lua
@@ -18,18 +18,22 @@ local function readlush(filename)
    local data = torch.ByteTensor(dims:storage())
    f:readByte(data:storage())
    f:close()
-   return data
+   return torch.Tensor(data:size()):copy(data)
 end
 
 local function createdataset(dataname, labelname)
    local data = readlush(dataname)
    local label = readlush(labelname)
    assert(data:size(1) == label:size(1))
-   local dataset = {data=data, label=label, size=data:size(1)}
-   setmetatable(dataset, {__index=function(self, idx)
-                                     assert(idx > 0 and idx <= self.size)
-                                     return {x=data[idx], y=label[idx]}
-                                  end})
+   for i = 1, label:size(1) do
+       if label[i] == 0 then label[i] = 10 end
+   end
+   local dataset = {data=data, label=label, size=function(self) return self.data:size(1) end}
+   setmetatable(dataset, {
+       __index=function(self, idx)
+           assert(idx > 0 and idx <= self.data:size(1))
+           return {data[idx]:resize(1, 28, 28), label[idx]}
+       end})
    return dataset
 end
 
